c16dcd0d16247731cf3481f7206960acdf36a2cb
Galatan Tudor <150925784+GalatzanT@users.noreply.github.com>
2025-04-16 12:25:14 +0300

    Add files via upload

---
+++ Lab6/Medicament.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <iostream>
+using std::string;
+using std::ostream;
+class Medicament {
+private:
+	int id;
+	string denumire;
+	double pret;
+	string producator;
+	string substantaActiva;
+public:
+	Medicament();
+	Medicament(int id, string denumire, double pret, string producator, string substantaActiva);
+	~Medicament();
+	Medicament(const Medicament& m) : id{ m.id }, denumire{ m.denumire }, pret{ m.pret }, producator{ m.producator }, substantaActiva{ m.substantaActiva } {
+		std::cout << "--Copy constructor called--\n";
+	};
+	int getId() const;
+	void setId(int id);
+	string getDenumire() const;
+	double getPret() const;
+	string getProducator() const;
+	string getSubstantaActiva() const;
+	void setDenumire(string denumire);
+	void setPret(double pret);
+	void setProducator(string producator);
+	void setSubstantaActiva(string substantaActiva);
+	bool operator==(const Medicament& medicament) const;
+	bool operator!=(const Medicament& medicament) const;
+	friend ostream& operator<<(ostream& os, const Medicament& medicament);
+};
+
+void testMedicament();
